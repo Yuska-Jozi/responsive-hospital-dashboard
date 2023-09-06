5619fdd3f5943d8682b6838651750fbe000ad9c9
Yuska-Jozi <68717878+Yuska-Jozi@users.noreply.github.com>
2023-09-06 07:47:37 +0000

    Completed wireframe of DT dashboard. Added functionality to menu and theme toggle switch. Built in media queries for different screen sizes.

---
+++ static/kpis.js
@@ -0,0 +1,26 @@
+const Kpis = [
+  {
+    Area: 'Staff',
+    KPI: 'Attendance',
+    UoM: '%',
+    Target: '>=95',
+    Actual: '94',
+    Indicator: 'Too low',    
+  },
+  {
+    Area: 'Equipment',
+    KPI: 'Mean Time Between Failures',
+    UoM: 'days',
+    Target: '>=90',
+    Actual: '143',
+    Indicator: 'In Spec',
+  },
+  {
+    Area: 'Planning',
+    KPI: 'PO Cycle Time',
+    UoM: '% of total',
+    Target: '>=75',
+    Actual: '95',
+    Indicator: 'In Spec',
+  }
+]
